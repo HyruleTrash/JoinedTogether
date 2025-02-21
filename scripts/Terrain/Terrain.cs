b5a1f2384f4a29411953e5acf726e41d34e1d9e8
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2025-02-21 13:36:03 +0100

    Update: Changed/added a project file structure

---
+++ scripts/Terrain/Terrain.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Terrain : TileMap
+{
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
